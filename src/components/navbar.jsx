4d1a2b6724ce2a7eda59f86d04e035715109be16
vandhanchowdhary <vandhanchowdhary@gmail.com>
2025-06-19 13:46:00 +0530

    styling changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,17 +2,21 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <header className="bg-blue-900 text-white flex flex-row justify-between h-[12%] items-center w-full fixed top-0 left-0 right-0 z-50 shadow-lg">
-      <div className="flex items-center pl-29">
-        <img
-          src="/logo.png"
-          alt="stacked books"
-          className=" bg-blue-300 rounded-[100%] w-14 h-14"
-        />
-        <h1 className="text-xl font-semibold p-2">Online Library System</h1>
-      </div>
-      <nav className="">
-        <div className="flex gap-6">
+    <header className="bg-blue-900 text-white fixed top-0 left-0 right-0 z-50 w-full shadow-md">
+      <div className="max-w-screen-xl mx-auto px-4 py-3 flex flex-col md:flex-row items-center justify-between gap-2">
+
+        <div className="flex items-center gap-3">
+          <img
+            src="/logo.png"
+            alt="stacked books"
+            className="bg-blue-300 rounded-full w-14 h-14"
+          />
+          <h1 className="text-lg md:text-xl font-semibold whitespace-nowrap">
+            Online Library System
+          </h1>
+        </div>
+
+        <nav className="flex gap-6 text-sm md:text-base py-1 border-b-1 border-t-1 md:border-none">
           <Link to="/" className="hover:underline">
             Home
           </Link>
@@ -22,11 +26,12 @@ const Navbar = () => {
           <Link to="/add" className="hover:underline">
             Add Book
           </Link>
-        </div>
-      </nav>
-      <p className="w-auto p-2 text-sm italic flex flex-wrap">
-        Brought to you by React + Redux
-      </p>
+        </nav>
+
+        <p className="text-xs italic text-center md:text-right">
+          Brought to you by React + Redux
+        </p>
+      </div>
     </header>
   );
 };
