fed2dad63f6827eb1f1e2e881951e1058c654fb8
vandhanchowdhary <vandhanchowdhary@gmail.com>
2025-06-19 13:02:59 +0530

    vite.config.js cahnged

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Online-Library-System/",
   plugins: [react(), tailwindcss()],
 });
